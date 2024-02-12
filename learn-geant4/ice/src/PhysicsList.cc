690d2690145bf663257b724ac8311d02aebb5394
Simon Thor <simon11.thor@hotmail.se>
2024-02-13 00:07:40 +0100

    Try to have neutrino physics in Geant4 11.1

--- learn-geant4/ice/src/PhysicsList.cc
+++ learn-geant4/ice/src/PhysicsList.cc
@@ -2,10 +2,10 @@
 /// \brief Implementation of the B1::PhysicsList class
 
 #include "PhysicsList.hh"
-// #include "G4DecayPhysics.hh"
-// #include "G4EmStandardPhysics.hh"
+#include "G4DecayPhysics.hh"
+#include "G4EmStandardPhysics.hh"
 // #include "G4RadioactiveDecayPhysics.hh"
-// #include "G4HadronPhysicsFTFP_BERT.hh"
+#include "G4HadronPhysicsFTFP_BERT.hh"
 // #include "G4EmExtraPhysics.hh"
 // TODO try to copy the relevant files related to G4NeutrinoPhysics and compile together with the rest
 #include "G4NeutrinoPhysics.hh"
@@ -15,15 +15,15 @@ namespace B1
 
 PhysicsList::PhysicsList()
 {
-  // RegisterPhysics(new G4DecayPhysics());
+  RegisterPhysics(new G4DecayPhysics());
   // RegisterPhysics(new G4RadioactiveDecayPhysics());
-  // RegisterPhysics(new G4EmStandardPhysics());
-  // RegisterPhysics(new G4HadronPhysicsFTFP_BERT());
+  RegisterPhysics(new G4EmStandardPhysics());
+  RegisterPhysics(new G4HadronPhysicsFTFP_BERT());
   G4NeutrinoPhysics* nuphys = new G4NeutrinoPhysics();
   // nuphys->NeutrinoActivated(true);
-  nuphys->NuETotXscActivated(true);
-  nuphys->SetNuDetectorName("DetectorLog");
-  nuphys->SetNuNucleusBias(1e14);
+  // nuphys->NuETotXscActivated(true);
+  // nuphys->SetNuDetectorName("DetectorLog");
+  // nuphys->SetNuNucleusBias(1e14);
   RegisterPhysics(nuphys);
   // nuphys->NeutrinoActivated(true);
   // nuphys->NuETotXscActivated(true);
