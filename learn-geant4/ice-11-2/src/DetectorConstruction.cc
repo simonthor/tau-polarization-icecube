3a3842dbb6c0511d304b4568fd8470a8d5f5a67b
Simon Thor <simon11.thor@hotmail.se>
2024-02-13 00:09:15 +0100

    Successfully run Geant4 with neutrino interactions! Using Geant4 11.2 and activating neutrinophysics

---
+++ learn-geant4/ice-11-2/src/DetectorConstruction.cc
@@ -0,0 +1,123 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file B1/src/DetectorConstruction.cc
+/// \brief Implementation of the B1::DetectorConstruction class
+
+#include "DetectorConstruction.hh"
+
+#include "G4RunManager.hh"
+#include "G4NistManager.hh"
+#include "G4Box.hh"
+#include "G4Cons.hh"
+#include "G4Orb.hh"
+#include "G4Sphere.hh"
+#include "G4Trd.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4Region.hh"
+#include "G4RegionStore.hh"
+
+namespace B1
+{
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+  // Get nist material manager
+  G4NistManager* nist = G4NistManager::Instance();
+
+  // Detector parameters
+  //
+  G4double env_sizeXY = 100.*m, env_sizeZ = 100.*m;
+  G4Material* env_mat = nist->FindOrBuildMaterial("G4_WATER");
+
+  // Option to switch on/off checking of volumes overlaps
+  //
+  G4bool checkOverlaps = true;
+
+  //
+  // World
+  //
+  G4double world_sizeXY = 1.2*env_sizeXY;
+  G4double world_sizeZ  = 1.2*env_sizeZ;
+  G4Material* world_mat = nist->FindOrBuildMaterial("G4_AIR");
+
+  auto solidWorld = new G4Box("World",                           // its name
+    0.5 * world_sizeXY, 0.5 * world_sizeXY, 0.5 * world_sizeZ);  // its size
+
+  auto logicWorld = new G4LogicalVolume(solidWorld,  // its solid
+    world_mat,                                       // its material
+    "World");                                        // its name
+
+  auto physWorld = new G4PVPlacement(nullptr,  // no rotation
+    G4ThreeVector(),                           // at (0,0,0)
+    logicWorld,                                // its logical volume
+    "World",                                   // its name
+    nullptr,                                   // its mother  volume
+    false,                                     // no boolean operation
+    0,                                         // copy number
+    checkOverlaps);                            // overlaps checking
+
+  //
+  // Detector
+  //
+  auto solidEnv = new G4Box("Detector",                    // its name
+    0.5 * env_sizeXY, 0.5 * env_sizeXY, 0.5 * env_sizeZ);  // its size
+
+  auto logicEnv = new G4LogicalVolume(solidEnv,  // its solid
+    env_mat,                                     // its material
+    "DetectorLog");                                 // its name
+
+  new G4PVPlacement(nullptr,  // no rotation
+    G4ThreeVector(),          // at (0,0,0)
+    logicEnv,                 // its logical volume
+    "DetectorPhys",               // its name
+    logicWorld,               // its mother  volume
+    false,                    // no boolean operation
+    0,                        // copy number
+    checkOverlaps);           // overlaps checking
+
+
+  G4Region* neutrinoInteractionRegion = new G4Region("DetectorLog");
+  logicEnv->SetRegion(neutrinoInteractionRegion);
+  neutrinoInteractionRegion->AddRootLogicalVolume(logicEnv);
+  // G4RegionStore::GetInstance()->Register(neutrinoInteractionRegion);
+  // Set water as scoring volume
+  //
+  fScoringVolume = logicEnv;
+
+  //
+  //always return the physical World
+  //
+  return physWorld;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+}
