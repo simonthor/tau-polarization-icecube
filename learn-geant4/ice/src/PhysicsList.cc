cee46b8adff4eaf22de85fb54d4614feba9d836e
Simon Thor <simon11.thor@hotmail.se>
2024-02-12 23:54:43 +0100

    Try to manually add neutrinophysics

--- learn-geant4/ice/src/PhysicsList.cc
+++ learn-geant4/ice/src/PhysicsList.cc
@@ -2,30 +2,34 @@
 /// \brief Implementation of the B1::PhysicsList class
 
 #include "PhysicsList.hh"
-#include "G4DecayPhysics.hh"
-#include "G4EmStandardPhysics.hh"
-#include "G4RadioactiveDecayPhysics.hh"
-#include "G4HadronPhysicsFTFP_BERT.hh"
-#include "G4EmExtraPhysics.hh"
+// #include "G4DecayPhysics.hh"
+// #include "G4EmStandardPhysics.hh"
+// #include "G4RadioactiveDecayPhysics.hh"
+// #include "G4HadronPhysicsFTFP_BERT.hh"
+// #include "G4EmExtraPhysics.hh"
 // TODO try to copy the relevant files related to G4NeutrinoPhysics and compile together with the rest
-// #include "G4NeutrinoPhysics.hh"
-// #include "G4HadronPhysicsNuBeam.hh"
+#include "G4NeutrinoPhysics.hh"
 
 namespace B1
 {
 
 PhysicsList::PhysicsList()
 {
-  RegisterPhysics(new G4DecayPhysics());
-  RegisterPhysics(new G4RadioactiveDecayPhysics());
-  RegisterPhysics(new G4EmStandardPhysics());
-  RegisterPhysics(new G4HadronPhysicsFTFP_BERT());
-  G4EmExtraPhysics* nuphys = new G4EmExtraPhysics();
-  nuphys->NeutrinoActivated(true);
+  // RegisterPhysics(new G4DecayPhysics());
+  // RegisterPhysics(new G4RadioactiveDecayPhysics());
+  // RegisterPhysics(new G4EmStandardPhysics());
+  // RegisterPhysics(new G4HadronPhysicsFTFP_BERT());
+  G4NeutrinoPhysics* nuphys = new G4NeutrinoPhysics();
+  // nuphys->NeutrinoActivated(true);
   nuphys->NuETotXscActivated(true);
   nuphys->SetNuDetectorName("DetectorLog");
-  nuphys->SetNuNucleusBias(1e34);
+  nuphys->SetNuNucleusBias(1e14);
   RegisterPhysics(nuphys);
+  // nuphys->NeutrinoActivated(true);
+  // nuphys->NuETotXscActivated(true);
+  // nuphys->SetNuDetectorName("DetectorLog");
+  // nuphys->SetNuNucleusBias(1e34);
+  // RegisterPhysics(nuphys);
   // RegisterPhysics(new G4HadronPhysicsNuBeam());
 }
 
