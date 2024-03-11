766110395352bb42991e0c91375c629df7a1c176
Simon Thor <simon11.thor@hotmail.se>
2024-03-11 18:27:24 +0100

    Add branching ratio function and make util functions easier to access

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .br import branching_ratios
+from .hepmc_io import csv2genevent, load_hepmc
---
+++ utils/br.py
@@ -0,0 +1,23 @@
+from collections import Counter
+import pandas as pd
+
+
+def branching_ratios(decay_products: pd.DataFrame) -> dict[tuple[int], float]:
+    """Compute branching ratios for the decay products of the tau lepton."""
+    n_taus = decay_products.query("pdg == 16").shape[0]
+    n_anti_taus = decay_products.query("pdg == -16").shape[0]
+    c = Counter(tuple(sorted(a.tolist())) for i, a in decay_products.groupby("event_num")["pdg"])
+    
+    br = {}
+    
+    for pdgs, n in c.items():
+        if 16 in pdgs:
+            br[pdgs] = n / n_taus
+        elif -16 in pdgs:
+            br[pdgs] = n / n_anti_taus
+        else:
+            raise ValueError("No tau in the event")
+    
+    # Sort based on the branching ratio. Highest first
+    br = dict(sorted(br.items(), key=lambda item: item[1], reverse=True))
+    return br
