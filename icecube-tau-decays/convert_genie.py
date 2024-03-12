c0ae82251b3218fbab624c91356769a99a809047
Simon Thor <simon11.thor@hotmail.se>
2024-03-12 16:33:02 +0100

    Copy csv2genevent to make the file self-contained

--- icecube-tau-decays/convert_genie.py
+++ icecube-tau-decays/convert_genie.py
@@ -7,10 +7,31 @@ import csv
 import pyhepmc
 import pandas as pd
 import numpy as np
-import sys
-sys.path.append("../")
-from utils.hepmc_io import csv2genevent
 from argparse import ArgumentParser
+
+
+def csv2genevent(event: pd.DataFrame, nucleus_row=0, nutau_row=1, tau_row=2) -> pyhepmc.GenEvent:
+    """This is a simplified version of the same function in plot_hepmc.
+    In that file, all decay products are included in the decay chain (including the electron that is not in the csv file)
+    Here, only the nucleus, neutrino and tau are included. This is because the tau will be decayed by Tauola."""
+    evt = pyhepmc.GenEvent(pyhepmc.Units.GEV, pyhepmc.Units.MM)
+    
+    # Extract particles        
+    nucleus = pyhepmc.GenParticle(pyhepmc.FourVector(*event.iloc[nucleus_row, 3:6], event.iloc[nucleus_row, 2]), event.iloc[nucleus_row, 1], 3)
+    nutau = pyhepmc.GenParticle(pyhepmc.FourVector(*event.iloc[nutau_row, 3:6], event.iloc[nutau_row, 2]), event.iloc[nutau_row, 1], 3)
+    # Set status to 1, since we want Tauola to decay it
+    tau = pyhepmc.GenParticle(pyhepmc.FourVector(*event.iloc[tau_row, 3:6], event.iloc[tau_row, 2]), event.iloc[tau_row, 1], 1)
+
+    nucleus_out4m = nucleus.momentum + nutau.momentum - tau.momentum
+    nucleus_out = pyhepmc.GenParticle(nucleus_out4m, 0, 1)
+    # Create vertices
+    interaction_vertex = pyhepmc.GenVertex()
+    interaction_vertex.add_particle_in(nucleus)
+    interaction_vertex.add_particle_in(nutau)
+    interaction_vertex.add_particle_out(tau)
+    interaction_vertex.add_particle_out(nucleus_out)
+    evt.add_vertex(interaction_vertex)
+    return evt
 
 
 def main():
