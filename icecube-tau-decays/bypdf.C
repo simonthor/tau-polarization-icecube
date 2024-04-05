acb6be85460e387a68dc05a198966548788009ae
Simon Thor <simon11.thor@hotmail.se>
2024-04-05 14:35:16 +0200

    Try BYPDF and compare

--- icecube-tau-decays/bypdf.C
+++ icecube-tau-decays/bypdf.C
@@ -5,6 +5,7 @@
 
 #include "Physics/PartonDistributions/PDFModelI.h"
 #include "Physics/PartonDistributions/PDF.h"
+#include "Physics/DeepInelastic/XSection/BYPDF.h"
 #include "Physics/PartonDistributions/GRV98LO.h"
 #include "Framework/ParticleData/PDGUtils.h"
 #include "Framework/Utils/KineUtils.h"
@@ -14,30 +15,34 @@
 using namespace genie;
 
 
-int crv98lo_pdf_eval(std::string output_file, double Q2) {
+int bypdf(std::string output_file, double Q2) {
+    BYPDF * bypdf = new BYPDF();
+    bypdf->Configure("Default");
+
     const PDFModelI * pdf_model =
-         dynamic_cast<const PDFModelI *> (new GRV98LO());
+         dynamic_cast<const PDFModelI *> (bypdf);
+    // std::cout << pdf_model->GetConfig() << "\n";
     PDF* fPDF =  new PDF();
-    
+    // std::cout << "Created PDF\n";
+
     fPDF->SetModel(pdf_model);
 
-    // Source: https://github.com/GENIE-MC/Generator/blob/1817b89cf815c0694c187f874288f1c1be1e712a/config/GRV98LO.xml#L11
-    // const double fQ2min = 0.800;
     // Charm mass, in GeV, according to Wikipedia
     const double fMc = 1.27;
     
     // Open output file and write header to it
     std::ofstream ofile(output_file);
     ofile << "x,Q2,fuv,fus,fdv,fds,fs,fc" << std::endl;
+    
+    // std::cout << "Opened file\n";
 
     // Iterate over an x from 0 to 1 with steps of 0.01
     for (double x = 0; x <= 1; x += 0.01) {
         // Reset PDF
         fPDF->Reset();
         // Calculate PDF at (x, Q2)
-        // double Q2pdf =  TMath::Max(Q2, fQ2min);
         fPDF->Calculate(x, Q2);
-        
+            
         double fuv =  fPDF  -> UpValence();
         double fus   = fPDF  -> UpSea();
         double fdv   = fPDF  -> DownValence();
