e0d29e9d38585c42f250b483a950a7d8fc3211ee
Simon Thor <simon11.thor@hotmail.se>
2024-04-22 13:14:23 +0200

    Add bash strict mode

--- icecube-tau-decays/dis_f_calc.sh
+++ icecube-tau-decays/dis_f_calc.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+# Bash strict mode:
+set -euo pipefail
+IFS=$'\n\t'
+# End of strict mode
+
 genie -l -b -q 'analytic_tau_pol_dis.cc("../data/test_genie_NuTau_5.0_GeV_event_info_dis.csv","../data/test_genie_NuTau_5.0_GeV_event_info_f.csv")'
 genie -l -b -q 'crv98lo_pdf.C("../data/test_genie_NuTau_5.0_GeV_event_info_f.csv","../data/test_genie_NuTau_5.0_GeV_event_info_pdf.csv")'
 
