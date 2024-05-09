ccb884891f49d8d8bfb55c00b6c5fbe9dd115835
Simon Thor <simon11.thor@hotmail.se>
2024-05-09 13:46:33 +0200

    Use for loop instead of copy paste. Also add res values

--- icecube-tau-decays/dis_f_calc.sh
+++ icecube-tau-decays/dis_f_calc.sh
@@ -3,18 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 # End of strict mode
+energies=(20 50 100)
 
-genie -l -b -q 'analytic_tau_pol_dis.cc("../data/test_genie_NuTau_5.0_GeV_event_info_dis.csv","../data/test_genie_NuTau_5.0_GeV_event_info_f.csv")'
-genie -l -b -q 'crv98lo_pdf.C("../data/test_genie_NuTau_5.0_GeV_event_info_f.csv","../data/test_genie_NuTau_5.0_GeV_event_info_pdf.csv")'
-
-genie -l -b -q 'analytic_tau_pol_dis.cc("../data/test_genie_NuTau_10.0_GeV_event_info_dis.csv","../data/test_genie_NuTau_10.0_GeV_event_info_f.csv")'
-genie -l -b -q 'crv98lo_pdf.C("../data/test_genie_NuTau_10.0_GeV_event_info_f.csv","../data/test_genie_NuTau_10.0_GeV_event_info_pdf.csv")'
-
-genie -l -b -q 'analytic_tau_pol_dis.cc("../data/test_genie_NuTau_20.0_GeV_event_info_dis.csv","../data/test_genie_NuTau_20.0_GeV_event_info_f.csv")'
-genie -l -b -q 'crv98lo_pdf.C("../data/test_genie_NuTau_20.0_GeV_event_info_f.csv","../data/test_genie_NuTau_20.0_GeV_event_info_pdf.csv")'
-
-genie -l -b -q 'analytic_tau_pol_dis.cc("../data/test_genie_NuTau_50.0_GeV_event_info_dis.csv","../data/test_genie_NuTau_50.0_GeV_event_info_f.csv")'
-genie -l -b -q 'crv98lo_pdf.C("../data/test_genie_NuTau_50.0_GeV_event_info_f.csv","../data/test_genie_NuTau_50.0_GeV_event_info_pdf.csv")'
-
-genie -l -b -q 'analytic_tau_pol_dis.cc("../data/test_genie_NuTau_100.0_GeV_event_info_dis.csv","../data/test_genie_NuTau_100.0_GeV_event_info_f.csv")'
-genie -l -b -q 'crv98lo_pdf.C("../data/test_genie_NuTau_100.0_GeV_event_info_f.csv","../data/test_genie_NuTau_100.0_GeV_event_info_pdf.csv")'
+for energy in "${energies[@]}"; do
+    genie -l -b -q "analytic_tau_pol_dis_int.C(\"../data/test_genie_NuTau_${energy}.0_GeV_event_info_dis.csv\",\"../data/test_genie_NuTau_${energy}.0_GeV_event_info_f.csv\")"
+    genie -l -b -q "crv98lo_pdf.C(\"../data/test_genie_NuTau_${energy}.0_GeV_event_info_f.csv\",\"../data/test_genie_NuTau_${energy}.0_GeV_event_info_pdf.csv\")"
+    genie -l -b -q "analytic_tau_pol_res_int.C(\"../data/test_genie_NuTau_${energy}.0_GeV_event_info_pdf.csv\",\"../data/test_genie_NuTau_${energy}.0_GeV_event_info_sig.csv\")"
+done
